928f1935cd1c84ea422e45f5cc6db2e6c3590ee6
DivyanshRai27 <divyanshrai27@gmail.com>
2020-06-01 15:55:13 +0530

    ejs setup

--- app.js
+++ app.js
@@ -5,14 +5,40 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req,res){
     var today = new Date();
+    var currentDay = today.getDay()
+    day = ""
 
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        res.send("Yay, its the weekend")
-    } else {
-        res.sendFile(__dirname + '/index.html')
-    }
+   switch (currentDay) {
+       case 0:
+           day="Sunday";
+           break;
+       case 1:
+            day="Monday";
+            break;
+         case 2:
+             day="Tuesday";
+            break;
+        case 3:
+            day="Wednesday";
+            break;
+        case 4:
+            day="Thursday";
+            break;
+        case 5:
+            day="Friday";
+            break;
+        case 6:
+            day="Saturday";
+            break;
+       default:
+            console.log();
+   }
+   
+    res.render("list", {kindOfDay: day})
 });
 
 app.listen(3000, function(){
